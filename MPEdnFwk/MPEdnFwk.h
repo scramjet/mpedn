7beada26714041f82486329f90fc458ed2095524
Gabriel Monteagudo <gabrielmaldi@hotmail.com>
2018-08-30 19:41:34 -0300

    Add framework target for Carthage

---
+++ MPEdnFwk/MPEdnFwk.h
@@ -0,0 +1,22 @@
+//
+//  MPEdnFwk.h
+//  MPEdnFwk
+//
+//  Created by gabrielm on 8/29/18.
+//  Copyright © 2018 Matthew Phillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MPEdnFwk.
+FOUNDATION_EXPORT double MPEdnFwkVersionNumber;
+
+//! Project version string for MPEdnFwk.
+FOUNDATION_EXPORT const unsigned char MPEdnFwkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPEdnFwk/PublicHeader.h>
+
+#import <MPEdnFwk/MPEdnParser.h>
+#import <MPEdnFwk/NSString+MPEdnParser.h>
+#import <MPEdnFwk/MPEdnWriter.h>
+#import <MPEdnFwk/MPEdnKeyword.h>
